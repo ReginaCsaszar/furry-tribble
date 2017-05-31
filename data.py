7d46681fc94e8a5d4e45fbaeb667efd27d4d8ea2
ReginaCsaszar <regina.csaszar@gmail.com>
2017-06-01 00:02:13 +0200

    add error handling to database handle function and database closing in every case

--- data.py
+++ data.py
@@ -4,16 +4,24 @@ from config import identify
 
 
 def get_results(query):
+    """Set up database connection, execute query and close all"""
     user = identify()
     connect_str = "dbname='{}' user='{}' host='localhost' password='{}'".format(user[0], user[1], user[2])
-    connection = psycopg2.connect(connect_str)
-    cursor = connection.cursor()
-    connection.autocommit = True
-    cursor = connection.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    cursor.close()
-    return rows
+    connection = False
+    try:
+        connection = psycopg2.connect(connect_str)
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        cursor.close()
+    except psycopg2.DatabaseError:
+        raise DatabaseError
+    else:
+        return rows
+    finally:
+        if connection:
+            connection.close()
 
 
 def main():
